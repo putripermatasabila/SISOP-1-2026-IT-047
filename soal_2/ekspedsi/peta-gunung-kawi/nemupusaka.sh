22f5e03ad72a2bbaeac1a353b42740e5467c56e9
putripermatasabila <putripermatasabila@gmail.com>
2026-03-18 12:31:16 +0700

    soal_2

---
+++ soal_2/ekspedsi/peta-gunung-kawi/nemupusaka.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+awk 'BEGIN {FS=","} /node_001|node_003/ {total_lat+=$3;total_long+=$4} 
+END {print "Koordinat pusat:";
+print total_long/2","total_lat/2}' titik-penting.txt>posisipusaka.txt
---
+++ soal_2/ekspedsi/peta-gunung-kawi/parserkoordinat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+awk '
+/"id":/ && /"node_/ { gsub(/.*"id": "|",/, "", $0); id=$0 }
+/"site_name":/ { gsub(/.*"site_name": "|",/, "", $0); site=$0 }
+/"latitude":/  { gsub(/.*"latitude": |,/, "", $0); lat=$0 }
+/"longitude":/ { gsub(/.*"longitude": |,/, "", $0); lon=$0; print id","site","lat","lon }
+' gsxtrack.json > titik-penting.txt
